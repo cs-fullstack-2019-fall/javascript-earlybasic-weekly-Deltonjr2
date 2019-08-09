e64ca75186ad1d76954d1187b36f0f1df2af2540
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-09 15:56:05 -0500

    project

---
+++ Index.js
@@ -0,0 +1,38 @@
+//Phase 1
+
+
+var userInput ; = "";
+var  userInput q=true
+while (userInputq)
+
+while user!="q"
+userInputq= prompt ( you entered ++"userInput + press 'q' to quit or enter another number");
+{ 
+alert ("you pressed 'q'");
+}
+{
+	! is userInput q
+}
+
+var Enteroption
+
+enterOption = prompt ("Choose 1 to scream choose 10 to skip"+ count >=1 )
+
+if (enterOption >1 then 1>=10}
+
+	console.log (enterOption);
+ }
+ Enteroption= prompt (" Choose if user wants to use + ")
+
+ if (user chooses a then ask them to enter a number )
+
+]console.log ( enter <=2)
+
+]
+
+
+//Phase 2
+
+var client 
+
+client = ( )
